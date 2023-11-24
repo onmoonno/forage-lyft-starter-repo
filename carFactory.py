60a5a5ac3a075c3403d70ea0a2fb2fc548c7241c
Yunxia Zhang <onvanessano@gmail.com>
2023-11-23 23:07:37 -0800

    unittest

--- carFactory.py
+++ carFactory.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from model.calliope import Calliope
 from model.glissade import Glissade
 from model.palindrome import Palindrome
@@ -8,26 +8,26 @@ from model.thovex import Thovex
 
 class CarFactory:
     @staticmethod
-    def create_calliope(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Calliope:
+    def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Calliope:
         car = Calliope(current_date, last_service_date, current_mileage, last_service_mileage)
         return car
 
     @staticmethod
-    def create_glissade(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Glissade:
+    def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Glissade:
         car = Glissade(current_date, last_service_date, current_mileage, last_service_mileage)
         return car
 
     @staticmethod
-    def create_palindrome(current_date: datetime, last_service_date: datetime, warning_light_is_on: bool) -> Palindrome:
+    def create_palindrome(current_date: date, last_service_date: date, warning_light_is_on: bool) -> Palindrome:
         car = Palindrome(current_date, last_service_date, warning_light_is_on)
         return car
 
     @staticmethod
-    def create_rorschach(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Rorschach:
+    def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Rorschach:
         car = Rorschach(current_date, last_service_date, current_mileage, last_service_mileage)
         return car
 
     @staticmethod
-    def create_thovex(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Thovex:
+    def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Thovex:
         car = Thovex(current_date, last_service_date, current_mileage, last_service_mileage)
         return car
